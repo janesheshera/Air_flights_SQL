590e9461b932daae3c0049221633f00335dcbeae
Evgeniia Popovich <63310859+janesheshera@users.noreply.github.com>
2022-08-20 18:49:15 +0300

    Delete 6_delay_aircrafts.sql

--- 6_delay_aircrafts.sql
+++
@@ -1,4 +0,0 @@
-select distinct model, max(actual_departure-scheduled_departure) over() as delay from flights
-join aircrafts on aircrafts.aircraft_code=flights.aircraft_code
-where actual_departure is not null 
-order by delay desc 
