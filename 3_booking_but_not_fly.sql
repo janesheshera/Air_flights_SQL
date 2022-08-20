03482eefc3ec687f3e0c2dce7a239e594ad242dc
Evgeniia Popovich <63310859+janesheshera@users.noreply.github.com>
2022-08-21 00:38:09 +0300

    Update 3_booking_but_not_fly.sql

--- 3_booking_but_not_fly.sql
+++ 3_booking_but_not_fly.sql
@@ -2,5 +2,3 @@ select count(book_ref) as qty_booking, count(boarding_no) as qty_boardingpass fr
 join tickets using(book_ref)
 join ticket_flights using(ticket_no)
 join boarding_passes using(flight_id)
-
-
