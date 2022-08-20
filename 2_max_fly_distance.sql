ebd5b17666d11288e69c32fa832e36a3a031ba0d
Evgeniia Popovich <63310859+janesheshera@users.noreply.github.com>
2022-08-20 18:48:24 +0300

    Delete 2_max_fly_distance.sql

--- 2_max_fly_distance.sql
+++
@@ -1,4 +0,0 @@
-select distinct airport_name, city, model, range from aircrafts
-join flights using(aircraft_code)
-join airports on flights.departure_airport=airports.airport_code
-where range = (select max(range) from aircrafts)
