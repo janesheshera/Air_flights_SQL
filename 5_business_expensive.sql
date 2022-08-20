6ae165ff6eb4ee226d94dce3edad3584fada1b48
Evgeniia Popovich <63310859+janesheshera@users.noreply.github.com>
2022-08-20 18:49:07 +0300

    Delete 5_business_expensive.sql

--- 5_business_expensive.sql
+++
@@ -1,7 +0,0 @@
-select city, departure_airport, arrival_airport, fare_conditions, amount, max_amount 
-from (
-select distinct city, departure_airport, arrival_airport, fare_conditions, amount, max(amount) over(partition by departure_airport, arrival_airport) as max_amount  from airports
-join flights on airports.airport_code=flights.arrival_airport
-join ticket_flights using(flight_id)
-) as d
-where fare_conditions = 'Business' and amount!=max_amount
